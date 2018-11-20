56e929bc01e8246cdba21bf9c08d37c3f1fe6de2
learn <eric.desimone@cortland.edu>
2018-11-19 20:41:36 -0500

    in controller validations and security checks to prevent the creation of an improper transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,20 +7,22 @@ class TransactionsController < ApplicationController
       end
     end
 
+    #prevent the addition of a new item_hash if the quantity is less than or equal to 0 and if the item quantity
     orders_attributes = orders_attributes.map do |order|
-         {item_quantities_attributes: order.map {|item| {item_id: Item.find_by(type_name: item[0]).id, quantity: item[1]} unless item[1].to_i <= 0}}
-    end
-    binding.pry
-
+         {item_quantities_attributes: order.map do |item|
+           if (item[1].to_i > 0) && ((Item.find_by(type_name: item[0]).quantity - item[1].to_i) >= 0)
+             {item_id: Item.find_by(type_name: item[0]).id, quantity: item[1].to_i}
+           end
+         end.compact
+       }
+    end.delete_if{|order| order[:item_quantities_attributes].empty? == true}
 
     @transaction = Transaction.create(store_id: Store.first.id,
       customer_id: params["customer_id"],
       orders_attributes: orders_attributes
        )
 
-     @transaction.orders.each do |order|
-
-     end
+      binding.pry
   end
 
 
